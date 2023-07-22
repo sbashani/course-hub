332c68aaee2f9f2ff4d9346431194ef98e571066
Samiththa Bashani <sami.bashani@gmail.com>
2023-07-22 10:09:59 -0400

    Rename the package.

---
+++ src/main/java/io/cms/coursehubapi/topic/TopicRepository.java
@@ -0,0 +1,6 @@
+package io.cms.coursehubapi.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+}
--- src/main/java/io/cms/coursehubapi/topics/TopicRepository.java
+++
@@ -1,6 +0,0 @@
-package io.cms.coursehubapi.topics;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, String> {
-}
